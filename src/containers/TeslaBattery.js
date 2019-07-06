84c53e7c5e546e22e3539ecfb0572080cda1d053
vasil-dimov <vasil.dimov.work@gmail.com>
2019-07-06 20:18:14 +0300

    Implement TeslaBattery basic component.

--- src/containers/TeslaBattery.js
+++ src/containers/TeslaBattery.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import './TeslaBattery.css';
+
 class TeslaBattery extends Component {
-    state = {  }
-    render() { 
-        return ( <div>TeslaBattery</div> );
+    state = {}
+    render() {
+        return (<form className="tesla-battery"><h1>Range Per Charge</h1></form>);
     }
 }
- 
+
 export default TeslaBattery;
